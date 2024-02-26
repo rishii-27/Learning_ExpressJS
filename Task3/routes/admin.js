eaa0911cf4f2fbd816a08aa30f71b90988b1c9f6
Hrishikesh <hrishikesh.joshi1997@gmail.com>
2024-02-26 16:03:00 +0530

    Task3: Filtering paths

--- Task3/routes/admin.js
+++ Task3/routes/admin.js
@@ -2,14 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   console.log("In add Product Middleware!");
   res.send(
-    '<form action="/product" method="POST"><div>Product:<input type="text" name="title">Size:<input type="number" name="size"><button type="submit">Add Product</button></div></form>'
+    '<form action="/admin/add-product" method="POST"><div>Product:<input type="text" name="title">Size:<input type="number" name="size"><button type="submit">Add Product</button></div></form>'
   );
 });
 
-router.post("/product", (req, res, next) => {
+// /admin/add-product => POST
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
