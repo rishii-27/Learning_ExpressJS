1ba20c1b5b96f7027869b76ff341fd593742b750
Hrishikesh <hrishikesh.joshi1997@gmail.com>
2024-03-05 16:23:56 +0530

    Task5: Using Helper function for Navigation

--- Task5/routes/admin.js
+++ Task5/routes/admin.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const path = require("path");
 
+const rootDir = require("../util/path");
+
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   console.log("In add Product Middleware!");
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // /admin/add-product => POST
